9a2c05aab99f7f68f275a502aa82e1ba38dd290e
Nandani <nks854338@gmail.com>
2025-01-26 04:30:45 +0530

    commited

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: "https://career-path-sand.vercel.app/", 
   credentials: true,
 };
 
